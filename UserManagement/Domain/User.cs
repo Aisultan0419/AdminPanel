21dc31d827351f338b122304bb9629da242af22e
Aisultan0419 <kalebekaisultan@gmail.com>
2026-06-09 17:24:41 +0500

    feat: Added domain part

---
+++ UserManagement/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagement.Domain
+{
+    public class User
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public UserStatus Status { get; set; } = UserStatus.Unverified;
+        public DateTime? LastActivityTime { get; set; }
+    }
+}
---
+++ UserManagement/Domain/UserStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagement.Domain
+{
+    public enum UserStatus
+    {
+        Unverified = 0,
+        Active = 1, 
+        Blocked = 2
+    }
+}
